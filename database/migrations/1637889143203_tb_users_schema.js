5356e98352c2ed00de6b6a9abbb8f9fa0f5864ac
Araya <joaoaraya7@gmail.com>
2021-11-26 02:12:11 +0000

    migração de tabelas

---
+++ database/migrations/1637889143203_tb_users_schema.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class TbUsersSchema extends Schema {
+  up () {
+    this.create('tb_users', (table) => {
+      table.increments().unique();
+      table.uuid("secure_id");
+      table.string("name", 30);
+      table.string("email", 60).unique().notNullable();
+      table.string("password", 30).notNullable();
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('tb_users')
+  }
+}
+
+module.exports = TbUsersSchema
